e8d24fadf4dea9c5668a6bd460a00f8a68ce368f
TaishiNishimura <taishi19880324@gmail.com>
2021-08-01 11:36:09 +0000

    8/1/20

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_062142) do
+ActiveRecord::Schema.define(version: 2021_08_01_111015) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
