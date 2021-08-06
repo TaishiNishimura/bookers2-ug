4a77cdbf067c23f5ee34b2aee3f6ffe8595279f3
EC2 Default User <ec2-user@ip-172-31-57-194.ec2.internal>
2021-08-06 03:20:47 +0000

    [7bfinish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,12 @@ class UsersController < ApplicationController
         @entry = Entry.new
       end
     end
-    @books = @user.books
     @book = Book.new
+    @books = @user.books.reverse_order
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
